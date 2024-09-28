f7ee027e2a475ca0b4f3178d56250a19a321ce63
samtheAI <104814157+samtheAI@users.noreply.github.com>
2024-09-28 11:19:37 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('Model Deployment: Logistic Regression')
+
+text = st.text_input("Enter the Text")
+
+if st.button("Predict"):
+
+     st.write("Thanks for sharing with us")
